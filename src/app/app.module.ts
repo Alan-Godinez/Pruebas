58de98db7e90108fb5c5bee518a467891b22299d
Alan-Godinez <alanzmbie572@gmail.com>
2020-03-03 17:10:16 -0600

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Numero48Component } from './numero48/numero48.component';
+import { Numero49Component } from './numero49/numero49.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Numero48Component,
+    Numero49Component
   ],
   imports: [
     BrowserModule
